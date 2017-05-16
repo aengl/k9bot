30db83d1cd5c552c6e34718cd4d2720c3f4c94fb
Andreas Smeria <ae@cephea.de>
2017-05-16 14:30:19 +0200

    Support direct messages & only respond to direct mentions

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const debug = require('debug')('index');
 const request = require('request-promise-native');
 const SlackBot = require('slackbots');
 
+const botName = 'k9';
 const bot = new SlackBot({
   token: process.env.BOT_TOKEN,
-  name: 'k9',
+  name: botName,
 });
+let botUser = undefined;
 
 const messageOptions = {
   as_user: true,
@@ -27,20 +29,55 @@ function getAnswer(question) {
       .then(res => {
         const data = JSON.parse(res);
         debug('got answers:', data);
-        resolve(data.answers[0].answer);
+        resolve(data.answers[0]);
       })
       .catch(debug);
   });
 }
 
-bot.on('message', data => {
-  // debug(data);
-  if (data.type === 'desktop_notification' && data.content) {
-    const mentionIndex = data.content.indexOf('@k9');
-    if (mentionIndex) {
-      const question = data.content.substr(mentionIndex + 4);
-      getAnswer(question).then(answer =>
-        bot.postMessage(data.channel, answer, messageOptions)
+function postAnswer(data, channel) {
+  debug('posting answer:', data);
+  bot.postMessage(
+    channel,
+    data.score > 0 ? data.answer : ':panda_face:',
+    messageOptions
+  );
+}
+
+function isChatMessage(message) {
+  return message.type === 'message' && Boolean(message.text);
+}
+
+function isDirectMessage(message) {
+  return message.channel && message.channel[0] === 'D';
+}
+
+function isFromMe(message) {
+  return message.user === botUser.id;
+}
+
+function mentionsMe(message) {
+  return message.text && message.text.indexOf(`<@${botUser.id}>`) >= 0;
+}
+
+function getTextWithoutMention(message) {
+  return message.text && message.text.replace(/<.+>/gi, '').trim();
+}
+
+bot.on('start', () => {
+  botUser = bot.users.find(user => user.name === botName);
+  debug('found myself on Slack:', botUser.id);
+});
+
+bot.on('message', message => {
+  if (isChatMessage(message) && !isFromMe(message)) {
+    if (isDirectMessage(message)) {
+      debug('I got a direct message:', message);
+      getAnswer(message.text).then(data => postAnswer(data, message.channel));
+    } else if (mentionsMe(message)) {
+      debug('I was mentioned in this message:', message);
+      getAnswer(getTextWithoutMention(message)).then(data =>
+        postAnswer(data, message.channel)
       );
     }
   }
