4b1eebacd42cd6893f7e722bf7e88005835724f1
Andreas Smeria <ae@cephea.de>
2017-05-25 19:09:36 +0200

    Added Procfile & cleanup

--- create.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-http POST https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/create Ocp-Apim-Subscription-Key:0d51bf1f017442c6bc7dffc3053981dd Content-Type:application/json < kb.json
